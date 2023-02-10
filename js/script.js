f67fa02c8e8df2dd22dd21eee35e0e6006968f00
FilDevTeam <dino-2003@mail.ru>
2023-02-10 17:52:23 +0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,6 @@
+let menu = document.querySelector('.menu');
+let menuin = document.querySelector('.menu-in');
+menu.addEventListener('click', function(){
+	menuin.classList.toggle('active');
+    menuin.classList.toggle('active');
+})
